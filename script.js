7eef40f45d2f838105463426845cc6980a59b6fa
Deepak Modanwal <118596712+deepakmod@users.noreply.github.com>
2023-04-24 12:31:12 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
 //your JS code here. If required.
+    window.onresize = displayWindowSize;
+    window.onload = displayWindowSize;
+
+    function displayWindowSize() {
+        let myWidth = window.innerWidth;
+        let myHeight = window.innerHeight;
+        let size=document.getElementById("size");
+        size.innerText="Width: "+myWidth+"px "+"Height: "+myHeight+"px"
+    }
